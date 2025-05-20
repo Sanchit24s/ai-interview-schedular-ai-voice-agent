57f7747824c7b4729ec56c0b0fb30d9a23de12bb
Sanchit24s <sanchit24bhoyar@gmail.com>
2025-05-20 19:14:42 +0530

    Implement Dashboard

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,12 +1,18 @@
+"use client";
 import React from 'react';
 import DashboardProvider from './provider';
+import Provider from '../provider';
 
 function DashboardLayout({ children }) {
     return (
         <div>
-            <DashboardProvider>
-                {children}
-            </DashboardProvider>
+            <Provider>
+                <DashboardProvider>
+                    <div className='p-10'>
+                        {children}
+                    </div>
+                </DashboardProvider>
+            </Provider>
         </div>
     );
 }
